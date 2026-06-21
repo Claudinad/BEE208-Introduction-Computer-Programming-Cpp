d9126be17feb1b4ad299249db31aae3b7845584c
ClaudinadOdai <odaiclaudinad@gmail.com>
2026-06-21 20:41:30 +0000

    Update README.m

--- Lab-02/README.m
+++ Lab-02/README.m
@@ -1,1 +1,12 @@
+# Lab 02 
 
+This lab contains solutions to c++ partical questions in Assignment 2.
+From Question 1 to Question 15
+
+Each question folder contains:
+- C++ source code
+- Algorithm
+- Pseudocode
+- Output screenshot
+- README
+- Flowchart
